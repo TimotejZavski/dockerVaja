7f1f68c0c8859f4de97aefbcfccd77506f40747a
TimotejZavski <timotej.zavski@tutanota.com>
2026-05-02 14:50:31 +0200

    fixan test?

--- test.sh
+++ test.sh
@@ -3,6 +3,11 @@
 output=$(python3 run.py)
 
 echo "$output" | grep "zagon python aplikacije" || exit 1
-echo "$output" | grep "konec" || exit 1
+echo "$output" | grep "odkrivam znanstveno fantastiko" || exit 1
+echo "$output" | grep "Korak 1" || exit 1
+echo "$output" | grep "Korak 2" || exit 1
+echo "$output" | grep "Korak 3" || exit 1
+echo "$output" | grep "konec :(" || exit 1
+echo "$output" | grep "lalalalalal" || exit 1
 
 echo "TEST OK"
